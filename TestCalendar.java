76473231736348f461c8dd663ce114e1220c32c8
jadilovic <adilovic79@yahoo.com>
2019-08-16 14:32:56 +0200

    day commit

--- TestCalendar.java
+++ TestCalendar.java
@@ -14,6 +14,23 @@ public class TestCalendar {
 		System.out.println("Current month is: " + calendar.get(Calendar.MONTH));
 		System.out.println("Current week is " + calendar.get(Calendar.WEEK_OF_YEAR));
 		System.out.println("Crrent day of the month is: " + calendar.get(Calendar.DAY_OF_MONTH));
+		System.out.println("Naziv dana u sedmici: " + calendar.get(Calendar.WEDNESDAY));
+		System.out.println("Current day of the week " + calendar.get(Calendar.DAY_OF_WEEK));
+		System.out.println("Current hour of the day" + calendar.get(Calendar.HOUR) + " hour: " + calendar.get(Calendar.HOUR_OF_DAY));
+		System.out.println("AM or PM: " + calendar.get(Calendar.AM_PM));
+		System.out.println("Day in the year: " + calendar.get(Calendar.DAY_OF_YEAR));
+		System.out.println("MINUTE: " + calendar.get(Calendar.MINUTE));
+		System.out.println("SECOND: " + calendar.get(Calendar.SECOND));
+		System.out.println("DAY_OF_WEEK: " + calendar.get(Calendar.DAY_OF_WEEK));
+		System.out.println("DAY_OF_MONTH: " + calendar.get(Calendar.DAY_OF_MONTH));
+		System.out.println("DAY_OF_YEAR: " + calendar.get(Calendar.DAY_OF_YEAR));
+		System.out.println("WEEK_OF_MONTH: " + calendar.get(Calendar.WEEK_OF_MONTH));
+		System.out.println("WEEK_OF_YEAR: " + calendar.get(Calendar.WEEK_OF_YEAR));
+		
+		Calendar calendar1 = new GregorianCalendar(2001, 8, 11);
+		String[] dayOfWeek = {"Sunday", "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday"};
+		System.out.println("Day of the week on September 11, 2001 is " + dayOfWeek[calendar1.get(Calendar.DAY_OF_WEEK)-1]);
+		System.out.println("Datum calendar1 is " + calendar1.get(Calendar.DAY_OF_MONTH) + ", " + calendar1.get(Calendar.MONTH) + ", " + calendar1.get(Calendar.YEAR));
 	}
 
 }

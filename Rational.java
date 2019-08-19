12178e225ee67caf961ec74b7af3c6b9efd43713
jadilovic <adilovic79@yahoo.com>
2019-08-19 09:04:12 +0200

    final commit

--- Rational.java
+++ Rational.java
@@ -1,5 +1,111 @@
 package Primjeri;
 
-public class Rational {
+public class Rational extends Number implements Comparable<Rational>{
 
+	private long numerator = 0;
+	private long denominator = 1;
+	
+	public Rational(){
+		this(0, 1);
+	}
+	
+	public Rational(long numerator, long denominator){
+		long gcd = gcd(numerator, denominator);
+		this.numerator = ((denominator > 0) ? 1 : -1) * numerator / gcd;
+		this.denominator = Math.abs(denominator) / gcd;
+	}
+	
+	private static long gcd(long numerator2, long denominator2) {
+		long num = Math.abs(numerator2);
+		long den = Math.abs(denominator2);
+		int gcd = 1;
+		
+		for(int k = 1; k <= num && k <= den; k++){
+			if(num % k == 0 && den % k == 0)
+				gcd = k;
+		}
+		return gcd;
+	}
+
+	public long getNumerator(){
+		return numerator;
+	}
+	
+	public long getDenominator(){
+		return denominator;
+	}
+	
+	public Rational add(Rational secondRational){
+		Rational addR = new Rational();
+		long addN = numerator * secondRational.denominator + denominator * secondRational.getNumerator();
+		long addD = denominator * secondRational.getDenominator();
+		addR = new Rational(addN, addD);
+		return addR;
+		
+	}
+	
+	public Rational subtract(Rational secondRational){
+		long addN = numerator * secondRational.denominator - denominator * secondRational.getNumerator();
+		long addD = denominator * secondRational.denominator;
+		return new Rational(addN, addD);
+	}
+	
+	public Rational multiply(Rational secondRational){
+		long addN = numerator * secondRational.getNumerator();
+		long addD = denominator * secondRational.getDenominator();
+		return new Rational(addN, addD);
+	}
+	
+	public Rational divide(Rational secondRational){
+		long addN = numerator * secondRational.getDenominator();
+		long addD = denominator * secondRational.getNumerator();
+		return new Rational(addN, addD);
+	}
+	
+	public String toString(){
+		if(denominator == 1)
+			return "" + numerator;
+		return "" + numerator + "/" + denominator;
+	}
+	
+	public boolean equals(Object other){
+		//if((this.subtract((Rational)(other))).getNumerator() == 0)
+		//	return true;
+		//else
+		//	return false;
+		return other.equals(this.doubleValue());
+	}
+	
+	@Override
+	public int compareTo(Rational o) {
+		if(this.subtract(o).getNumerator() > 0)
+			return 1;
+		if(this.subtract(o).getNumerator() < 0)
+			return -1;
+		else
+		return 0;
+	}
+
+	@Override
+	public double doubleValue() {
+		return numerator * 1.0 / denominator;
+	}
+
+	@Override
+	public float floatValue() {
+		return (float) doubleValue();
+	}
+
+	@Override
+	public int intValue() {
+		return (int) doubleValue();
+	}
+
+	@Override
+	public long longValue() {
+		// TODO Auto-generated method stub
+		return (long) doubleValue();
+	}
+
+	
 }
--- testRationalClass.java
+++ testRationalClass.java
@@ -5,6 +5,39 @@ public class testRationalClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Rational ra1 = new Rational(4, 2);
+		Rational ra2 = new Rational(2, 3);
+		Rational ra3 = new Rational(4, 2);
+		
+		System.out.println(ra1.add(ra2));
+		System.out.println(ra1.toString());
+		System.out.println(ra1 + " - " + ra2 +  " = " + ra1.subtract(ra2));
+		System.out.println(ra1 + " - " + ra2 +  " = " + ra1.multiply(ra2));
+		System.out.println(ra1 + " - " + ra2 +  " = " + ra1.divide(ra2));
+		System.out.println(ra1 + " to double " + ra1.doubleValue() + " " + ra2 + " to double " + ra2.doubleValue());
+		System.out.println(ra1.compareTo(ra2));
+		
+		Rational r1 = new Rational(1, 123456789);
+		Rational r2 = new Rational(1, 123456789);
+		Rational r3 = new Rational(1, 123456789);
+		System.out.println("r1 * r2 * r3 is " + r1.multiply(r2.multiply(r3)));
+		
+		Rational rat1 = new Rational(-2, 6);
+		System.out.println(rat1.getNumerator());
+		System.out.println(rat1.getDenominator());
+		System.out.println(rat1.intValue());
+		System.out.println(rat1.doubleValue());
+		
+		Rational rr1 = new Rational(-2, 6);
+		Object rr2 = new Rational(1, 45);
+		System.out.println(r2.compareTo(r1));
+		
+		System.out.println(ra1.equals(ra2));
+		System.out.println(ra3.equals(ra1));
+		
+		Rational rat4 = new Rational(1, 2);
+		Rational rat5 = new Rational(1, -2);
+		System.out.println(rat4.add(rat5));
 	}
 
 }
